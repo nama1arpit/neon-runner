924c213fb42be32079bf1eea5c0c3013c013a940
Arpit Nama <anam9745@uni.sydney.edu.au>
2021-04-14 18:21:43 +0530

    Added level 1 structure

---
+++ data/maps/blue_tiles.tsx
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="blue_tiles" tilewidth="64" tileheight="64" tilecount="7" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="64" height="64" source="../images/blue_tile_0.png"/>
+ </tile>
+ <tile id="1">
+  <image width="64" height="64" source="../images/blue_tile_1.png"/>
+ </tile>
+ <tile id="2">
+  <image width="64" height="64" source="../images/blue_tile_2.png"/>
+ </tile>
+ <tile id="3">
+  <image width="64" height="64" source="../images/blue_tile_3.png"/>
+ </tile>
+ <tile id="4">
+  <image width="64" height="64" source="../images/blue_tile_4.png"/>
+ </tile>
+ <tile id="7">
+  <image width="64" height="64" source="blue_coin.png"/>
+ </tile>
+ <tile id="8">
+  <image width="64" height="64" source="blue_bolt.png"/>
+ </tile>
+</tileset>
